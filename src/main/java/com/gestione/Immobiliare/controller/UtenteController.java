c24b02cebd6db6b404d3d5c53cbf21070e23e513
Tdavide04 <tdavide833@gmail.com>
2025-10-24 10:46:48 +0200

    aggiunta configurazione per il Cors e modificato il ritorno del JWT

--- src/main/java/com/gestione/Immobiliare/controller/UtenteController.java
+++ src/main/java/com/gestione/Immobiliare/controller/UtenteController.java
@@ -1,19 +1,20 @@
 package com.gestione.Immobiliare.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestione.Immobiliare.dto.UtenteDTO;
 import com.gestione.Immobiliare.security.JwtUtil;
 import com.gestione.Immobiliare.service.IUtenteService;
+import com.gestione.Immobiliare.utility.LoginRequest;
 
 @RestController
 @RequestMapping("users")
@@ -38,17 +39,24 @@ public class UtenteController {
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity loginUtente(@RequestParam String email, @RequestParam String password) {
+	public ResponseEntity loginUtente(@RequestBody LoginRequest request) {
 		try {
-			Optional<UtenteDTO> utenteDto = utenteService.trovaLogin(email, password);
+			Optional<UtenteDTO> utenteDto = utenteService.trovaLogin(request.getEmail(), request.getPassword());
+			
 			if (utenteDto.isPresent()) {
 				String token = jwtUtil.generateToken(utenteDto.get().getEmail());
-	            return ResponseEntity.ok().body(token); 
+				Map<String, String> response = new HashMap<>();
+	            response.put("token", token);
+	            return ResponseEntity.ok(response); 
 	        } else {
-	            return ResponseEntity.status(404).body("Utente non trovato o credenziali errate");
+	        	 Map<String, String> response = new HashMap<>();
+	             response.put("error", "Utente non trovato o credenziali errate");
+	             return ResponseEntity.status(404).body(response);
 	        }
 		} catch (Exception e) {
-			return ResponseEntity.badRequest().body("Errore durante la login: " + e.getMessage());
+			Map<String, String> response = new HashMap<>();
+	        response.put("error", "Errore durante la login: " + e.getMessage());
+	        return ResponseEntity.badRequest().body(response);
 		}
 	}
 }
